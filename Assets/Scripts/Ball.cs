ec7b136dac3d2ff03ab01f41b99b95ace262fcfa
Murray Butler <murray.butler@M083910BUG3QD.community.veritas.com>
2017-12-22 14:55:27 -0600

    Updates to have player move and ball response.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,44 +6,45 @@ using UnityEngine.Networking;
 public class Ball : NetworkBehaviour {
 
 	public float StartSpeed = 5f;
-	public float MaxSpeed = 20f;
-	public float SpeedIncrease = 0.25f;
-	public GameObject ballPrefab;
+	public float MaxSpeed = 100f;
+	public float SpeedIncrease = 20f;
 
 	private float currSpeed;
 	private Vector3 currDir;
 	private bool resetter = false;
-	private Rigidbody RBod;
-
+	//private float xRange = 9f;
+	//private float zRange = 30f;
 
 	// Use this for initialization
 	void Start () {
 		currSpeed = StartSpeed;
 		currDir = Random.insideUnitCircle.normalized;
-		RBod = GetComponent<Rigidbody> ();
 	}
 
-	public override void OnStartServer () {
-		var ball = (GameObject)Instantiate (ballPrefab);
-		NetworkServer.Spawn (ball);
-	}
+	//public override void OnStartServer () {
+	//	var ball = (GameObject)Instantiate (ballPrefab);
+	//	NetworkServer.Spawn (ball);
+	//}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (resetter) {
 			return;
 		}
+			
 		Vector3 mvDir = currDir * currSpeed * Time.deltaTime;
-		//transform.Translate (new Vector3 (mvDir.x, 0f, mvDir.y));
-		RBod.AddForce(mvDir.x,0f,mvDir.z);
+		//Vector3 ballPos = new Vector3(Mathf.Clamp(mvDir.x, -xRange, xRange), 1f, Mathf.Clamp(mvDir.z,-zRange,zRange));
+		transform.Translate (new Vector3 (mvDir.x, 0f, mvDir.z));
 	}
 
 	void OnTriggerEnter(Collider coll) {
-		if (coll.tag == "Wall") {
-			currDir.y *= -1;
-		} else if(coll.tag == "Playa") {
-			currDir.x *= -1;
-		} else if(coll.tag == "Goal") {
+		//if (coll.tag == "Wall") {
+		//	currDir.x *= -1;
+		//} else if(coll.tag == "Playa") {
+		//	currDir.x *= -1;
+		//} else if(coll.tag == "Goal") {
+		if (coll.tag == "Goal") {
+			Destroy (this);
 			StartCoroutine (resetBall ());
 			coll.SendMessage("GetPoint", SendMessageOptions.DontRequireReceiver);
 		}
@@ -56,28 +57,13 @@ public class Ball : NetworkBehaviour {
 		resetter = true;
 		transform.position = Vector3.zero;
 
-		currDir = Vector3.zero;
-		currSpeed = 0f;
-		yield return new WaitForSeconds(2f);
+		//currDir = Vector3.zero;
+		//currSpeed = 0f;
+		yield return new WaitForSeconds(1f);
 
 		Start();
 
 		resetter = false;
 	}
-
-	void OnCollisionEnter(Collision coll) {
-		if (coll.gameObject.tag == "Wall") {
-			currDir.y *= -1;
-			Debug.Log ("Ball Collided with " + coll.collider.name);
-		} else if(coll.gameObject.tag == "Playa") {
-			currDir.x *= -1;
-			Debug.Log ("Ball Collided with " + coll.collider.name);
-		} else if(coll.gameObject.tag == "Goal") {
-			StartCoroutine (resetBall ());
-			coll.gameObject.SendMessage("GetPoint", SendMessageOptions.DontRequireReceiver);
-		}
-
-		currSpeed += SpeedIncrease;
-		currSpeed = Mathf.Clamp (currSpeed, StartSpeed, MaxSpeed);
-	}
+		
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,14 @@ using UnityEngine.Networking;
 public class PlayerController : NetworkBehaviour {
 
 	public float MvSpeed = 10f;
-	public float MvRange = 9f;
+	public float xRange = 9f;
+	public float zRange = 30f;
 
-	//private Vector3 readNetPos;
-	//private Rigidbody RBod;
 	private Rigidbody ball;
 	private Vector3 playaPos = new Vector3 (30, 1, 10);
 
 	void Start() {
-		//RBod = GetComponent<Rigidbody> ();
-		//ball = GameObject.Find("Ball").GetComponent<Rigidbody>();
+		transform.Rotate(90,0,0);
 	}
 
 	void Update()
@@ -26,45 +24,17 @@ public class PlayerController : NetworkBehaviour {
 			return;
 		}
 
-		//float var x = Input.GetAxis ("Mouse X") * Time.deltaTime * 150.0f;
-		//float var z = Input.GetAxis ("Mouse Y") * Time.deltaTime * 3.0f;
-
 		float xPos = transform.position.x + (Input.GetAxis ("Mouse X") * MvSpeed);
 		float zPos = transform.position.z + (Input.GetAxis ("Mouse Y") * MvSpeed);
 
-		//var z = Input.GetAxis ("Vertical") * Time.deltaTime * 150.0f;
-		//var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 3.0f;
+		playaPos = new Vector3(Mathf.Clamp(xPos, -xRange, xRange), 1.05f, Mathf.Clamp(zPos,-zRange,0f));
 
-		//float xPos = transform.position.x + (Input.GetAxis ("Horizontal") * MvSpeed);
-		//float zPos = transform.position.z + (Input.GetAxis ("Vertical") * MvSpeed);
-		playaPos = new Vector3(Mathf.Clamp(xPos, -9f, 9f), 1.05f, Mathf.Clamp(zPos,-30f,0f));
-		//playaPos = new Vector3(xPos, 1.05f, zPos);
 		transform.position = playaPos;
 
-		//Vector3 pos = transform.localPosition;
-		//pos.z += z * MvSpeed * Time.deltaTime;
-		//pos.x += x * MvSpeed * Time.deltaTime;
-		//pos.z = z;
-		//pos.x = x;
-
-		//pos.z = Mathf.Clamp (pos.z, -MvRange, MvRange);
-		//pos.x = Mathf.Clamp (pos.x, -MvRange, MvRange);
-		//pos.y = 0f;
-
-		//transform.position = pos;
-
-		//transform.Rotate(x,0,0);
-		//transform.Translate(x,z,0);
-
-		//playaPos = Input.mousePosition;
-
-		//curPos.z = 10.0f;
-		//curPos.x = 1.0f;
-		//curPos = Camera.main.ScreenToWorldPoint (curPos);
+		transform.Rotate(90,0,0);
 
 		//Debug.Log (curPos);
 
-		//RBod.MovePosition(curPos);
 	}
 
 	void OnSerializeNetworkView(BitStream str) {
